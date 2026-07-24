967f152b2b61573ab6366b3b2bb6af7736e16185
Gabriel Bispo <gabrielsbisppo@gmail.com>
2026-07-23 21:47:52 -0300

    adiciona método __str__ à classe carro para exibir informações detalhadas

--- exercicios/carro.py
+++ exercicios/carro.py
@@ -4,3 +4,6 @@ class carro(Veiculo):
     def __init__(self, marca, modelo, portas):
         super().__init__(marca, modelo)
         self.portas = portas
+
+    def __str__(self):
+        return f'{super().__str__()} | Portas: {self.portas}'
